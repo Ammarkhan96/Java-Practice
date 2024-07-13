43d13b6ab784ebc0a848211b7b333cee6c456a44
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-07-14 00:33:30 +0500

    first commit

---
+++ ProjectSix/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //String
+
+//        String message = "Hello World";
+        //string with concatenation
+        String message = "Hello World" + "!!";
+
+//        System.out.println(message);
+//        System.out.println(message.endsWith("!!"));
+//        System.out.println(message.startsWith("!!"));
+        System.out.println(message.length());
+    }
+}
