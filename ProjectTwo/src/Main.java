23f95e73bf2179e1cdd8cc57dabd7a07cf65db14
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-07-13 19:21:25 +0500

    first commit

---
+++ ProjectTwo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//VARIABLES:
+//       int age = 30;
+//        age = 35;
+
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
