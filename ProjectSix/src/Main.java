59f9904d3818949451e0a23dd713259611455616
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-07-16 20:06:26 +0500

    first commit

--- ProjectSix/src/Main.java
+++ ProjectSix/src/Main.java
@@ -9,6 +9,12 @@ public class Main {
 //        System.out.println(message);
 //        System.out.println(message.endsWith("!!"));
 //        System.out.println(message.startsWith("!!"));
-        System.out.println(message.length());
+//        System.out.println(message.length());
+//        System.out.println(message.indexOf("H"));
+//        System.out.println(message.indexOf("e"));
+//        Pass any character or a string that doesn't exist:
+//        System.out.println(message.indexOf("sky"));
+        System.out.println(message.replace("!",  "*"));
+        System.out.println(message);
     }
 }
