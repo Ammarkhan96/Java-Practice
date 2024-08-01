4f20fb0f5303406079585a12c6d2bf83ed2c2e4b
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-08-02 00:21:41 +0500

    first commit

---
+++ ProjectNine(arthematicExpression)/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+//         Arthimetic expressions:
+
+//        int result = 10 + 3;
+//        int result = 10 - 3;
+//        int result = 10 * 3;
+//        double result = (double)10 / (double)3;
+
+        //increment and decrement
+//        int x = 1;
+//        x++;
+//        ++x;
+
+//        int x = 1;
+//        int y = x++;
+//        System.out.println(x);
+
+//        int x = 1;
+//        int y = ++x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+        int x = 1;
+        x += 2;
+        System.out.println(x);
+    }
+}
--- ProjectSeven(arrays)/src/Main.java
+++ ProjectSeven(arrays)/src/Main.java
@@ -28,7 +28,12 @@ public class Main {
 
         //MULTI-DIMENSIONAL ARRAYS:
 
-        int[] numbers = new int[5];
-        System.out.println();
+//        int[][] numbers = new int[2][3];
+//        numbers[0][0] = 1;
+////        System.out.println(Arrays.toString(numbers));
+//        System.out.println(Arrays.deepToString(numbers));
+
+        int [][] numbers = { {1, 2, 3}, {4, 5, 6} };
+           System.out.println(Arrays.deepToString(numbers));
     }
 }
