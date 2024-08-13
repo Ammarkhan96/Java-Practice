f2570ed2d2b031f06e5629236917449d95cc50df
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-08-14 00:08:24 +0500

    first commit

---
+++ ProjectTwelve(theMathClass)/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+//        int result = Math.round(1.1F);
+
+//        int result = (int)Math.ceil(1.1F);
+
+//        int result = (int)Math.floor(1.1F);
+
+//        int result = Math.max(1, 2);
+
+//        int result = Math.min(1, 2);
+
+//        double result = Math.random();
+
+//        double result = Math.round(Math.random() * 100);
+
+        int result = (int) Math.random() * 100;
+
+        System.out.println(result);
+    }
+}
