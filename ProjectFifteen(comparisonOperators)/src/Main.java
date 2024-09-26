5dc1108b9fc4d471ff878ad41ba822d4497939dd
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-09-26 12:34:39 +0500

    first commit

---
+++ ProjectFifteen(comparisonOperators)/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        int x = 1;
+//        int y = 1;
+      // this expression is boolean expression:
+        //System.out.println(x==y);
+
+        //inequality operator:
+//        System.out.println(x!=y);
+//        System.out.println(x>=y);
+
+        int x = 1;
+        int y = 2;
+        System.out.println(x<=y);
+    }
+}
